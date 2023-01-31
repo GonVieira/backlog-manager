37276bc2510c778c2199ab26463407a1fd5bb95d
GonVieira <vieiragoncalo23@gmail.com>
2023-01-31 00:28:35 +0000

    Project setup.

---
+++ src/pages/Homepage/Homepage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Homepage = () => {
+
+    return (
+        
+        <h2>Homepage</h2>
+        
+    )
+}
+
+export default Homepage;
---
+++ src/pages/Homepage/style.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const HomepageComponentBody = styled.div`
+    
+
+`
