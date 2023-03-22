03640ebbb09ea93cd99af6f3aae41eb5a91948ee
GonVieira <vieiragoncalo23@gmail.com>
2023-03-22 19:43:36 +0000

    Login functionality.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,4 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import { deleteCookie } from "../utils/cookies";
 
